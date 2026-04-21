e225d6a50aaf046baa5c9a792fa93defd44c3db5
denisunderonov <denisunderonov2@gmail.com>
2026-04-21 13:07:10 +0300

    Создал кастомные типы и функции для работы со списком задач

--- lessons.ts
+++ lessons.ts
@@ -1,15 +1,48 @@
-let projectName: string;
-let version: number;
-let isActive: boolean;
-let currentTaskId: number | null;
-let strornot: unknown;
-
-if (typeof strornot === 'string') {
-    console.log(strornot.toUpperCase());
-} else {
-    console.log('strornot is not a string');
+type Task = {
+    id: number;
+    title: string;
+    done: boolean;
 }
 
-function printProjectInfo(projectName: string, version: number): void { 
-    console.log(`${projectName} v${version}`);
+let tasks: Task[] = []
+let currentTaskId: number = 0;
+
+function createTask(taskTitle: string): Task {
+    let task: Task = {
+        id: currentTaskId++,
+        title: taskTitle,
+        done: false,
+    };
+    return task;
 }
+
+function addTask(tasks: Task[], task: Task): void {
+    tasks.push(task);
+    console.log(`Task ${task.title} added`);
+}
+
+function markDone(tasks: Task[], taskId: number): void {
+    let task = tasks.find(el => el.id === taskId);
+    if (task) {
+        task.done = true;
+        console.log(`Task ${task.title} marked as done`);
+    } else {
+        console.log(`Task with id ${taskId} not found`);
+    }
+}
+
+function listTasks(tasks: Task[]): void {
+    console.log('Tasks:');
+    tasks.forEach(task => {
+        console.log(`[${task.title}] / [${task.done ? 'Done' : 'Not done'}]`);
+    });
+    console.log(`Total tasks: ${tasks.length}`);
+}
+
+let newTask = createTask('Learn TypeScript');
+let newTask2 = createTask('Learn JavaScript');
+addTask(tasks, newTask);
+addTask(tasks, newTask2);
+listTasks(tasks);
+markDone(tasks, 1);
+listTasks(tasks);
